2542f493f3db3e09f09f63050161bf991ccfe642
Sinacam <stinkingmadgod@gmail.com>
2019-11-27 00:50:37 +0800

    Namespace kills the function

--- typestring.hpp
+++ typestring.hpp
@@ -6,25 +6,25 @@ namespace typestring
     inline constexpr auto nameof()
     {
     #if defined(__clang__)
-        // 0         1         2
-        // 012345678901234567890123456789
-        // auto nameof() [T = T]
-        auto sv = std::string_view(__PRETTY_FUNCTION__);
-        return sv.substr(19, sv.size() - 20);
-    #elif defined(__GNUC__) || defined(__GNUG__)
         // 0         1         2         3
         // 0123456789012345678901234567890123456789
-        // constexpr auto nameof() [with T = T]
+        // auto typestring::nameof() [T = T]
         auto sv = std::string_view(__PRETTY_FUNCTION__);
-        return sv.substr(34, sv.size() - 35);
+        return sv.substr(31, sv.size() - 32);
+    #elif defined(__GNUC__) || defined(__GNUG__)
+        // 0         1         2         3         4
+        // 01234567890123456789012345678901234567890123456789
+        // constexpr auto typestring::nameof() [with T = T]
+        auto sv = std::string_view(__PRETTY_FUNCTION__);
+        return sv.substr(46, sv.size() - 47);
     #elif defined(_MSC_VER)
         // MSVC has this great idea that we need to know
         // if a type is a union, enum, struct or class.
         // 0         1         2         3
         // 0123456789012345678901234567890123456789
-        // auto __cdecl nameof<T>(void)
+        // auto __cdecl typestring::nameof<T>(void)
         auto sv = std::string_view(__FUNCSIG__);
-        sv = sv.substr(20, sv.size() - 27);
+        sv = sv.substr(32, sv.size() - 39);
         if(sv.find("struct ") != std::string_view::npos)
             return sv.substr(7);
         if(sv.find("class ") != std::string_view::npos)
